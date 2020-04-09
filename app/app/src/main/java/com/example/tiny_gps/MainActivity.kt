ff4ffd96cf3e1e4ba1b58c4f35d0e9244daaad39
Jerry Liu <jerrylarryliu@gmail.com>
2020-04-09 14:08:47 -0700

    update app

--- app/app/src/main/java/com/example/tiny_gps/MainActivity.kt
+++ app/app/src/main/java/com/example/tiny_gps/MainActivity.kt
@@ -1,25 +1,37 @@
 package com.example.tiny_gps
 
-import com.example.tiny_gps.ApClass
-
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ListActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
+import com.example.tiny_gps.com.example.tiny_gps.ApClass
+import android.os.Handler
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+//var box : ApClass = ApClass("0", "0", booleanArrayOf(false, false, false, false, false, false))
 
 class MainActivity : AppCompatActivity() {
-
-
-
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("hello world")
+        (this as AppCompatActivity).supportActionBar?.title = "GPS"
+
+
     }
 
 
@@ -29,4 +41,42 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
