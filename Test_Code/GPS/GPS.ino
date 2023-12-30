c48b6c621b68b507865c47327736ea8e5d814ba8
liuyal <jerrylarryliu@gmail.com>
2023-12-29 17:23:42 -0800

    update test code

--- Test_Code/GPS/GPS.ino
+++ Test_Code/GPS/GPS.ino
@@ -5,21 +5,30 @@
 #include "TinyGPS++.h"
 
 #define DEBUG true
+
+#ifdef DEBUG
+#define DEBUG_PRINT(x) Serial.print(x);
+#else
+#define DEBUG_PRINT(x) \
+  do { \
+  } while (0);
+#endif
+
+#ifdef DEBUG
+#define DEBUG_PRINT_LN(x) Serial.println(x);
+#else
+#define DEBUG_PRINT_LN(x) \
+  do { \
+  } while (0);
+#endif
+
 #define LED_PIN 22
 #define RXD2 16
 #define TXD2 17
 
 TinyGPSPlus gps;
-File myFile;
 
 const int CS_PIN = 5;
-
-void logger(String msg, bool nl = true) {
-#if DEBUG
-  if (nl) { msg = msg + "\n"; }
-  Serial.print(msg);
-#endif
-}
 
 
 /**********************************************************************
@@ -27,59 +36,48 @@ void logger(String msg, bool nl = true) {
  **********************************************************************/
 
 void SD_INIT() {
-  logger("\n\rInitializing SD Card...");
+  DEBUG_PRINT_LN("\n\rInitializing SD Card...");
 
   if (!SD.begin(CS_PIN)) {
-    logger("SD Card Initialization Failed!");
-    delay(1000);
+    DEBUG_PRINT_LN("SD Card Initialization Failed!");
     return;
   }
 
-  logger("-------SD Card Info-------");
+  DEBUG_PRINT_LN("-------SD Card Info-------");
   uint8_t cardType = SD.cardType();
   uint64_t bytes = SD.totalBytes();
   uint64_t used_bytes = SD.usedBytes();
 
-  if (cardType == CARD_MMC) logger("SD Card Type:\tMMC");
-  else if (cardType == CARD_SD) logger("SD Card Type:\tSDSC");
-  else if (cardType == CARD_SDHC) logger("SD Card Type:\tSDHC");
-  else if (cardType == CARD_NONE) logger("SD Card Type:\tNo SD Card Attached");
-  else logger("UNKNOWN");
+  if (cardType == CARD_MMC) {
+    DEBUG_PRINT_LN("SD Card Type:\tMMC");
+  } else if (cardType == CARD_SD) {
+    DEBUG_PRINT_LN("SD Card Type:\tSDSC");
+  } else if (cardType == CARD_SDHC) {
+    DEBUG_PRINT_LN("SD Card Type:\tSDHC");
+  } else if (cardType == CARD_NONE) {
+    DEBUG_PRINT_LN("SD Card Type:\tNo SD Card Attached");
+  } else {
+    DEBUG_PRINT_LN("UNKNOWN");
+  }
 
-  logger("Used(MB):\t" + String((float)used_bytes / (1000 * 1000)));
-  logger("Used(GB):\t" + String((float)used_bytes / (1000 * 1000 * 1000)));
-  logger("Volume(MB):\t" + String((float)bytes / (1000 * 1000)));
-  logger("Volume(GB):\t" + String((float)bytes / (1000 * 1000 * 1000)));
-  logger("--------------------------");
+  DEBUG_PRINT("Used(MB):\t");
+  DEBUG_PRINT_LN((float)used_bytes / (1000 * 1000));
+  DEBUG_PRINT("Used(GB):\t");
+  DEBUG_PRINT_LN((float)used_bytes / (1000 * 1000 * 1000));
+  DEBUG_PRINT("Volume(MB):\t");
+  DEBUG_PRINT_LN((float)bytes / (1000 * 1000));
+  DEBUG_PRINT("Volume(GB):\t");
+  DEBUG_PRINT_LN((float)bytes / (1000 * 1000 * 1000));
+  DEBUG_PRINT_LN("--------------------------");
 }
 
+void appendFile(fs::FS &fs, String message) {
+  File file = fs.open("/test.log", FILE_APPEND);
+  if (!file) return;
+  file.print(message);
+  file.close();
+}
 
-// void appendFile(fs::FS &fs, String path, String message) {
-//   File file = fs.open(path, FILE_APPEND);
-//   if (!file) return;
-//   file.print(message);
-//   file.close();
-// }
-
-// String readFile(fs::FS &fs, String path) {
-//   String text = "";
-//   logger("Reading file: " + path + "\n\r");
-//   File file = fs.open(path);
-//   if (!file) {
-//     return "Failed to open file for reading\n\r";
-//   }
-//   while (file.available()) {
-//     text = text + (char)file.read();
-//   }
-//   file.close();
-//   return text;
-// }
-
-// void deleteFile(fs::FS &fs, String path) {
-//   logger("Deleting file: " + path + "\n\r");
-//   if (fs.remove(path)) logger("File deleted\n\r");
-//   else logger("Delete failed\n\r");
-// }
 
 /**********************************************************************
   Main
@@ -93,13 +91,13 @@ void setup() {
   // Initialize Serial
   Serial.begin(115200);
 
-  // Initialize Serial to NEO
+  // Initialize HW Serial to NEO
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
 
   // Initialize SD Card
   SD_INIT();
 
-  logger("Initialization Complete!");
+  DEBUG_PRINT_LN("Initialization Complete!");
 }
 
 void loop() {
@@ -110,18 +108,17 @@ void loop() {
   while (Serial2.available()) {
     int raw_data = Serial2.read();
     gps.encode(raw_data);
-    gnss_data = String(gnss_data + String((char)raw_data));
+    gnss_data += (char)raw_data;
   }
 
   if (gps.location.isValid()) {
     digitalWrite(LED_PIN, HIGH);
-    // appendFile(SD, "/" + gnss_dir + "/GPS_" + String(nfiles) + ".log", log_buffer);
+    DEBUG_PRINT(gnss_data)
+    appendFile(SD, gnss_data);
 
   } else {
     digitalWrite(LED_PIN, LOW);
-    // logger("No GNSS FIX...");
-    // delay(30000);
+    DEBUG_PRINT_LN("No GNSS FIX...");
+    delay(1000);
   }
-
-  logger(gnss_data, false);
 }
